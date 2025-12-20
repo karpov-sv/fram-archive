374e499874c63b41546ffdf29928e2ba299d52c0
karpov-sv <karpov.sv@gmail.com>
2025-12-20 14:00:27 +0100

    Properly set module path for deployment

--- archive/wsgi.py
+++ archive/wsgi.py
@@ -7,7 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+path = os.path.abspath(__file__)
+path = os.path.split(path)[0]
+path = os.path.split(path)[0]
+
+sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
