d6872f67dcf1de1c84f6f65a395be850c34b89b2
karpov-sv <karpov.sv@gmail.com>
2026-05-20 12:06:55 +0200

    Add query parameter template tag

--- archive/templatetags/tags.py
+++ archive/templatetags/tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.urls import get_script_prefix
+from urllib.parse import urlencode
 
 register = template.Library()
 
@@ -8,3 +9,16 @@ register = template.Library()
 def get_root(context):
     #return context['request'].path
     return get_script_prefix()
+
+
+@register.simple_tag
+def encode_params(**kwargs):
+    params = {}
+
+    for key, value in kwargs.items():
+        if value is None or value == '' or value == 'all':
+            continue
+
+        params[key] = value
+
+    return urlencode(params)
